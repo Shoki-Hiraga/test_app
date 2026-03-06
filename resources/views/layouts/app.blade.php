ea2bcdd2e1643d290ae9c4f5c91893a83d98e47c
shoki-hiraga <chaser.cb750@gmail.com>
2026-03-06 19:02:53 +0900

    CSS整理

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{ $title ?? 'Laravel App' }}</title>
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+  
 </head>
 <body>
   <header class="topbar">
